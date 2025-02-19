eb75f778bb2eb570174924ac1717549765e863a3
yu3473 <sanyaldebeshro@gmail.com>
2025-02-19 20:57:40 +0530

    Add files via upload

---
+++ pictures/pictures.js
@@ -0,0 +1,3 @@
+var target = document.getElementsByClassName("instagram");
+
+target[0].innerHTML = '<div class="display"><div class="logo"><div class="container"><div class="camera"><div class="flash"></div><div class="lens"></div></div></div></div><div class="text"><div class="text-container">Follow</div></div></div>';
